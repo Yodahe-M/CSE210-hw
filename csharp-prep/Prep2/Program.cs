bd07e6d39c8cad3d925006543d51177ca5b6c9c9
Yodahe Mulugeta <mul21017@byui.edu>
2023-01-07 20:38:33 -0700

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+       
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed this course!");
+        }
+        else 
+        {
+            Console.WriteLine("You have to retake this class next semester. You got this!");
+        }
+
+        
+
+
     }
 }
