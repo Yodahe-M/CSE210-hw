ec6dcbaa4e5478bef66d1f6504d2a90af93d2d72
Yodahe Mulugeta <mul21017@byui.edu>
2023-01-11 19:28:28 -0700

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Let's Play a guessing game");
+
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+
+        //Console.Write("What is the magic number? ");
+        //string magicNumber = Console.ReadLine();
+        //int magic = int.Parse(magicNumber);
+
+
+
+        Console.Write("What is your guess? ");
+        string guessNumber = Console.ReadLine();
+        int guess = int.Parse(guessNumber);
+
+
+    while (guess != magicNumber)
+    {
+
+    
+
+        //guess = int.Parse(Console.ReadLine());
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("Lower");
+        }
+
+       
+        Console.Write("What is your guess? ");
+        guessNumber = Console.ReadLine();
+        guess = int.Parse(guessNumber);
+
+    }
+     Console.WriteLine("You guessed it!");
+
+    
+       
     }
 }
