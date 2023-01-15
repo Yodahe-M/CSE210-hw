39c5b99b837ac50b56906222d0302d57b2a64b46
Yodahe Mulugeta <mul21017@byui.edu>
2023-01-14 20:27:42 -0700

    Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,57 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
+
+        void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            
+
+            return name;
+        }
+
+        int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number? ");
+            string number = Console.ReadLine();
+            int favNumber = int.Parse(number);
+
+            // int number = int.Parse(Console.ReadLine());
+            
+
+            return favNumber;
+        }
+
+        int SquareNumber(int favNumber)
+        {
+            int square = favNumber * favNumber;
+            return square;
+        }
+
+
+        void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+
+
+
     }
 }
