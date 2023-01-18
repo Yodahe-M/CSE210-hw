a3efe8ce5d34e1fe01e9462696e68e3f74ac8f0c
Yodahe Mulugeta <mul21017@byui.edu>
2023-01-18 14:35:36 -0700

    Resume

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,19 @@
+public class Resume
+{
+    public string _givenName;
+    public string _familyName;
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayResumeDetails()
+    {
+        Console.WriteLine($"Name: {_givenName} {_familyName}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+
+    }
+}
